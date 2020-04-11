3d5577d8dc1a1c8b5d0c8ec8d64746220455f186
Sasha <khovinalexa@gmail.com>
2020-04-10 20:41:00 -0700

    initial set up for aws rds

---
+++ src/main/java/com/example/springbootawssetup/SpringbootAwsSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootawssetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSetupApplication.class, args);
+	}
+
+}
