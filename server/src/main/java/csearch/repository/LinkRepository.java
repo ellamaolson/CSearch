86a8ba805f86d5022c125182377f520d887db278
Elana Olson <elanaolson@gmail.com>
2020-04-28 13:34:48 -0700

    cleanup imports for Link Repo

--- server/src/main/java/csearch/repository/LinkRepository.java
+++ server/src/main/java/csearch/repository/LinkRepository.java
@@ -1,12 +1,11 @@
 package csearch.repository;
 
-
 import csearch.model.Link;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+
 @Repository("lrepo")
 public interface LinkRepository extends CrudRepository<Link, Integer> {
 
