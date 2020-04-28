15f8d20320fca54f0a6174c2ca2f42df7bb19b3c
Elana Olson <elanaolson@gmail.com>
2020-04-28 13:28:26 -0700

    add search query to repository

--- server/src/main/java/csearch/repository/CsRepository.java
+++ server/src/main/java/csearch/repository/CsRepository.java
@@ -4,11 +4,18 @@ import csearch.model.CsProject;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+<<<<<<< HEAD
 import java.util.List;
 
 @Repository("csrepo")
 public interface CsRepository extends CrudRepository<CsProject, Integer>{
     @Query(value = "SELECT pid, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
+=======
+
+@Repository("csrepo")
+public interface CsRepository extends CrudRepository<CsProject, Integer>{
+    @Query(value = "SELECT id, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
+>>>>>>> add search query to repository
       "%:searchTerm% OR cs.description LIKE %:searchTerm% OR cs.process LIKE %:searchTerm%", nativeQuery = true)
     List<CsProject> findProjectMatchingSearchTerm(String searchTerm);
 }
