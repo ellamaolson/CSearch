4c826e461d15c109ac453dcba60ba894a69a5dd9
Elana Olson <elanaolson@gmail.com>
2020-04-28 13:04:25 -0700

    cors change to any origin

--- server/src/main/java/csearch/controller/CsController.java
+++ server/src/main/java/csearch/controller/CsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CsController {
     private final CsRepository csRepo;
 
