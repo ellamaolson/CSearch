bafe15fe46473d5095d99103e15007f0072f3322
Sasha <khovinalexa@gmail.com>
2020-04-29 14:20:43 -0700

    rebased with master

--- server/src/main/java/csearch/repository/CsRepository.java
+++ server/src/main/java/csearch/repository/CsRepository.java
@@ -4,26 +4,14 @@ import csearch.model.CsProject;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.util.List;
 
-@Repository("csrepo")
-public interface CsRepository extends CrudRepository<CsProject, Integer>{
-    @Query(value = "SELECT pid, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
-=======
 
 @Repository("csrepo")
 public interface CsRepository extends CrudRepository<CsProject, Integer>{
-    @Query(value = "SELECT id, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
->>>>>>> add search query to repository
-=======
-import java.util.List;
+      @Query(value = "SELECT pid, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
+        "%:searchTerm% OR cs.description LIKE %:searchTerm% OR cs.process LIKE %:searchTerm%", nativeQuery = true)
+      List<CsProject> findProjectMatchingSearchTerm(String searchTerm);
 
-@Repository("csrepo")
-public interface CsRepository extends CrudRepository<CsProject, Integer>{
-    @Query(value = "SELECT pid, title, description, process, difficulty FROM csprojects cs WHERE cs.title LIKE " +
->>>>>>> add and fix searching
-      "%:searchTerm% OR cs.description LIKE %:searchTerm% OR cs.process LIKE %:searchTerm%", nativeQuery = true)
-    List<CsProject> findProjectMatchingSearchTerm(String searchTerm);
 }
